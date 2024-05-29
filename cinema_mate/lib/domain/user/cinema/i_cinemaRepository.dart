bec66c09c844b10491713c3926f258b84ba2756b
Iman-301 <150245996+Iman-301@users.noreply.github.com>
2024-05-29 23:52:02 +0300

    Update i_cinemaRepository.dart

--- cinema_mate/lib/domain/user/cinema/i_cinemaRepository.dart
+++ cinema_mate/lib/domain/user/cinema/i_cinemaRepository.dart
@@ -2,7 +2,7 @@ import 'package:cinema_mate/domain/cinema/cinema.dart';
 import 'package:cinema_mate/domain/cinema/cinemaFailure.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class IMovieRepository{
+abstract class ICinemaRepository{
   Stream<Either<CinemaFailure, List<Cinema>>> watchAll();
   Stream<Either<CinemaFailure, Cinema>> getCinemaDescription(Cinema cinema);
   Future<Either<CinemaFailure, Unit>> checkoutMovie(Cinema cinema);
